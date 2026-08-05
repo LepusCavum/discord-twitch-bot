eac1e5d3ade69428d7f0daf066023fdb19328144
Megan Joy <87344696+LepusCavum@users.noreply.github.com>
2026-08-05 22:09:23 +0000

    Adding initial Logging testing

---
+++ tests/DiscordTwitchBot.Tests/Logging/LoggingExtensionsTests.cs
@@ -0,0 +1,52 @@
+using DiscordTwitchBot.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace DiscordTwitchBot.Tests.Logging;
+
+// TODO : WHY ARE THESE NOT BEING DETECTED?!?!?!?!
+
+public class LoggingExtensionsTests
+{
+    public void LoggingExtension_ConfiguresApplicationLogging()
+    {
+        // Arrange
+        var builder = Host.CreateApplicationBuilder();
+        builder.Logging.SetMinimumLevel(LogLevel.None);
+
+        // Act
+        builder.Logging.AddLogging(builder.Environment);
+        using var host = builder.Build();
+        var options = host.Services.GetRequiredService<IOptions<LoggerFilterOptions>>().Value;
+
+        // Assert
+        Assert.Equal(LogLevel.Warning, options.MinLevel);
+
+    }
+
+    public void LoggingExtension_ConfiguresDevelopmentEnvLogging()
+    {
+        // Arrange
+        var builder = Host.CreateApplicationBuilder(
+            new HostApplicationBuilderSettings
+            {
+                EnvironmentName = Environments.Development
+            }
+        );
+        builder.Logging.SetMinimumLevel(LogLevel.None);
+
+        // Act
+        builder.Logging.AddLogging(builder.Environment);
+        using var host = builder.Build();
+        var options = host.Services.GetRequiredService<IOptions<LoggerFilterOptions>>().Value;
+        LoggerFilterRule? rule = options.Rules
+            .FirstOrDefault(rule =>
+                rule.CategoryName == "DiscordTwitchBot");
+
+        // Assert
+        Assert.NotNull(rule);
+        Assert.Equal(LogLevel.Debug, rule.LogLevel);
+    }
+}
