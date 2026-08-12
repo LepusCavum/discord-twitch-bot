742f8d384751fe4631e2c555f8e6f4ae6baf40bf
LepusCavum <lepuscavum418@gmail.com>
2026-08-12 14:45:38 -0700

    Add Startup Logging test

--- tests/DiscordTwitchBot.Tests/Services/StartupServiceTests.cs
+++ tests/DiscordTwitchBot.Tests/Services/StartupServiceTests.cs
@@ -22,5 +22,20 @@ public class StartupServiceTests
         // Assert
         Assert.Null(exception);
     }
+    
+    [Fact]
+    public async Task StartupService_LogsStartupInformation()
+    {
+        // Arrange
+        var logger = new TestLogger<StartupService>();
+        var host = Host.CreateApplicationBuilder().Build();
+        var service = new StartupService(host.Services.GetRequiredService<IHostApplicationLifetime>(), logger, host.Services.GetRequiredService<IHostEnvironment>());
+
+        // Act
+        await service.StartAsync(CancellationToken.None);
+
+        // Assert
+        Assert.Contains(logger.Entries, log => log.Message.Contains("Application starting"));
+    }
 
 }
