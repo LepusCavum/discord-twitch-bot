6afdb4d5778a0a7e644e3e31418c0dd7e786e139
LepusCavum <lepuscavum418@gmail.com>
2026-08-14 11:45:01 -0700

    Clarify logging statements

--- src/DiscordTwitchBot/Services/StartupService.cs
+++ src/DiscordTwitchBot/Services/StartupService.cs
@@ -29,14 +29,22 @@ public class StartupService : IStartupService, IHostedService
     public Task StartAsync(CancellationToken cancellationToken)
     {
         try {
-            _applicationLifetime.ApplicationStopping.Register(() => _logger.LogInformation("Application is stopping. Cancellation requested? {tokenRequested}", cancellationToken.IsCancellationRequested));
             
             var version = Assembly.GetExecutingAssembly().GetName().Version;
 
-            _logger.LogInformation("Application starting: {ApplicationName} v{Version} in {Environment}. Cancellation requested? {tokenRequested}",
+            _logger.LogInformation("Application starting: {ApplicationName} v{Version} in {Environment}. App cancellation requested? {tokenRequested}",
                 _environment.ApplicationName, 
                 version, 
-                _environment.EnvironmentName, cancellationToken.IsCancellationRequested);
+                _environment.EnvironmentName, _applicationLifetime.ApplicationStopping.IsCancellationRequested);
+            _logger.LogInformation("StartupService is starting. Service cancellation requested? {tokenRequested}",
+                cancellationToken.IsCancellationRequested);
+
+            _applicationLifetime.ApplicationStopping.Register(() => 
+                _logger.LogInformation("Application is stopping. App cancellation requested? {tokenRequested}", 
+                _applicationLifetime.ApplicationStopping.IsCancellationRequested));
+            _applicationLifetime.ApplicationStopped.Register(() => 
+                _logger.LogInformation("Application is stopped. App cancellation requested? {tokenRequested}", 
+                _applicationLifetime.ApplicationStopping.IsCancellationRequested));
 
             return Task.CompletedTask;
         } catch (Exception ex) {
@@ -47,7 +55,7 @@ public class StartupService : IStartupService, IHostedService
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
-        _logger.LogInformation("Application is stopped. Cancellation requested? {tokenRequested}", 
+        _logger.LogInformation("StartupService is stopped. Service cancellation requested? {tokenRequested}", 
             cancellationToken.IsCancellationRequested);
                 
         return Task.CompletedTask;
